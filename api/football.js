3bbf9cf4feb2a433f3daea369f1beace16869a27
cosgroryan <r.cosgrove@hotmail.com>
2026-06-17 16:46:12 +1200

    more

--- api/football.js
+++ api/football.js
@@ -1,12 +1,12 @@
 import https from 'node:https';
 
 export default function handler(req, res) {
-  const apiPath = req.url.replace(/^\/api\/football/, '');
+  // req.url is the original request path e.g. /api/football/competitions/2000/matches?status=FINISHED
+  const apiPath = req.url.replace(/^\/api\/football/, '') || '/';
   const url = `https://api.football-data.org/v4${apiPath}`;
 
   https.get(url, { headers: { 'X-Auth-Token': process.env.VITE_FOOTBALL_API_KEY || '' } }, (r) => {
     res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
     res.statusCode = r.statusCode;
     r.pipe(res);
   }).on('error', (e) => {
